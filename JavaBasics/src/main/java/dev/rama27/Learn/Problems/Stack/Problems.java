9effd79c5c472cdcc174b7ce7ffc4052aa89d11b
rama-27 <ramarajumantena231@gmail.com>
2025-04-01 15:29:40 +0530

    squash! hmmm

--- JavaBasics/src/main/java/dev/rama27/Learn/Problems/Stack/Problems.java
+++ JavaBasics/src/main/java/dev/rama27/Learn/Problems/Stack/Problems.java
@@ -6,7 +6,8 @@ public class Problems {
     public static void main(String[] args){
 
 //        reverseStack("rama");
-        
+
+//        checkValidParanthesis("(((****)");
 
     }
 
@@ -23,4 +24,32 @@ public class Problems {
 
     }
 
+    public static boolean checkValidParanthesis(String s) {
+        int minOpen = 0;
+        int maxOpen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (c == ')') {
+                minOpen--;
+                maxOpen--;
+            } else if (c == '*') {
+                minOpen--;
+                maxOpen++;
+            }
+
+            if (maxOpen < 0) {
+                return false;
+            }
+
+            minOpen = Math.max(0, minOpen);
+        }
+
+        System.out.println(minOpen==0);
+        return minOpen == 0;
+    }
 }
